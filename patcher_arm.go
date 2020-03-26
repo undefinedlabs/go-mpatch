2f95d3265dca89f2cc12cfc513e57763042a287b
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2020-03-26 09:43:23 +0100

    Apply review feedback

--- patcher_arm.go
+++
@@ -1,8 +0,0 @@
-// +build arm
-
-package mpatch
-
-// Gets the jump function rewrite bytes
-func getJumpFuncBytes(to uintptr) ([]byte, error) {
-	return nil, errors.New(fmt.Sprintf("Unsupported architecture: %s", runtime.GOOS))
-}
--- patcher_unsupported.go
+++ patcher_unsupported.go
@@ -1,13 +1,15 @@
-// +build arm64
+// +build !386
+// +build !amd64
 
 package mpatch
 
 import (
 	"errors"
+	"fmt"
 	"runtime"
 )
 
 // Gets the jump function rewrite bytes
 func getJumpFuncBytes(to uintptr) ([]byte, error) {
-	return nil, errors.New(fmt.Sprintf("Unsupported architecture: %s", runtime.GOOS))
+	return nil, errors.New(fmt.Sprintf("Unsupported architecture: %s", runtime.GOARCH))
 }
